ffa15761bfff0c085520911a1b8fa65c16308e25
Genesis John Abobo <geiizanityy@gmail.com>
2024-01-24 13:09:13 +0800

    Remove with function in ProjectResource

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -7,16 +7,9 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProjectResource extends JsonResource
 {
-    public static $wrap = 'project';
     public function toArray(Request $request): array
     {
         return parent::toArray($request);
-    }
-    public function with(Request $request)
-    {
-        return [
-            'message' => "d"
-        ];
     }
 
 }
