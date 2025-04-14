c5cf9a5222045ab1fa332eeaa1e1916c5028f691
eptals1 <196735565+eptals2@users.noreply.github.com>
2025-04-14 17:04:24 +0800

    [UPDATE] added softdeletes

---
+++ database/migrations/2025_04_14_084547_add_soft_deletes_attachments.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //Add soft deletes to attachments table
+        Schema::table('attachments', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //Remove soft deletes from attachments table
+        Schema::table('attachments', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+};
---
+++ database/migrations/2025_04_14_085453_add_soft_deletes_project_activity_logs.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //Add soft deletes to project_activity_logs table
+        Schema::table('project_activity_logs', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //Remove soft deletes from project_activity_logs table
+        Schema::table('project_activity_logs', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+};
