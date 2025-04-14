868f24917930580217d9a181df3fe5dcd96b2327
eptals1 <196735565+eptals2@users.noreply.github.com>
2025-04-14 17:11:12 +0800

    [UPDATE] 2. use SoftDeletes traits on the model.

--- app/Models/HrmsUser.php
+++ app/Models/HrmsUser.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class HrmsUser extends Model implements AuthenticatableContract
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     public function getAuthIdentifierName()
     {
