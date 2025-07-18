1aaede93127bd5ff1bc4a7c6fa75740639125ac7
eptals1 <eptals2@gmail.com>
2025-07-18 13:30:24 +0800

    FilterPhaseRequest → FilterBOQPartRequest

--- app/Http/Requests/Phase/FilterBOQPartRequest.php
+++ app/Http/Requests/Phase/FilterBOQPartRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Phase;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class FilterPhaseRequest extends FormRequest
+class FilterBOQPartRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
