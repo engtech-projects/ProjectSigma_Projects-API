8c9bc9a37ede125ba3ebc3de4e3e5086d99fabe9
eptals1 <eptals2@gmail.com>
2025-07-21 11:39:11 +0800

    FilterTaskRequest → FilterBoqItemRequest

--- app/Http/Requests/Task/FilterBoqItemRequest.php
+++ app/Http/Requests/Task/FilterBoqItemRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Task;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class FilterTaskRequest extends FormRequest
+class FilterBoqItemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
