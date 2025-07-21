3a37969e624649f1f563563118b9d11dedda4a4d
eptals1 <eptals2@gmail.com>
2025-07-21 11:50:00 +0800

    Rename StoreTaskRequest to StoreBoqItemRequest

--- app/Http/Requests/BoqItem/StoreBoqItemRequest.php
+++ app/Http/Requests/BoqItem/StoreBoqItemRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Task;
+namespace App\Http\Requests\BoqItem;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreTaskRequest extends FormRequest
+class StoreBoqItemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
