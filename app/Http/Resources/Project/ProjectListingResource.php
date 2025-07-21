fa7d4c709f6becdd024db0108dd05d57ad950127
eptals1 <eptals2@gmail.com>
2025-07-21 13:47:25 +0800

    pint

--- app/Http/Resources/Project/ProjectListingResource.php
+++ app/Http/Resources/Project/ProjectListingResource.php
@@ -3,8 +3,6 @@
 namespace App\Http\Resources\Project;
 
 use App\Enums\TssStage;
-use App\Http\Resources\Approvals\ApprovalAttributeCollection;
-use App\Http\Resources\BoqPart\BoqPartCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProjectListingResource extends JsonResource
