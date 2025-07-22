77b97012c87390399d2ba377cf6014a87be9b493
eptals1 <eptals2@gmail.com>
2025-07-22 14:40:14 +0800

    Update BoqItemResource.php

--- app/Http/Resources/Project/BoqItemResource.php
+++ app/Http/Resources/Project/BoqItemResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Project;
 
+use App\Http\Resources\DupaDetailedResource;
 use App\Http\Resources\ResourceItem\ResourceItemCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -20,19 +21,7 @@ class BoqItemResource extends JsonResource
             'amount' => $this->amount,
             'total_price' => $this->total_price,
             'unit_price_with_quantity' => $this->unit_price_with_quantity,
-            'resources' => $this->whenLoaded('resources', function () {
-                return [
-                    'direct_cost_data' => $this->each_resource_item_total,
-                    'ocm' => $this->ocm,
-                    'contractors_profit' => $this->contractors_profit,
-                    'vat' => $this->vat,
-                    'grand_total' => $this->grand_total,
-                    'direct_cost' => $this->resource_item_total,
-                    'unit_cost_per' => $this->unit_cost_per,
-                    'data' => ResourceItemCollection::collection($this->resources),
-                ];
-            }),
-
+            'resources' => DupaDetailedResource::collection($this->whenLoaded('resources')),
         ];
     }
 }
