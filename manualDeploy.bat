d51f85eabab8156b0adf5eec744cf86e1ead6c69
Jose Janlofre Dy <janlofredy@gmail.com>
2025-08-01 14:53:30 +0800

    Update manualDeploy.bat

--- manualDeploy.bat
+++ manualDeploy.bat
@@ -1,6 +1,6 @@
-composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
-./vendor/bin/pint --test
-@REM php artisan key:generate
-php artisan migrate --step --force
-php artisan db:seed
-php artisan optimize
+call composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
+call ./vendor/bin/pint --test
+@REM call php artisan key:generate
+call php artisan migrate --step --force
+call php artisan db:seed
+call php artisan optimize
