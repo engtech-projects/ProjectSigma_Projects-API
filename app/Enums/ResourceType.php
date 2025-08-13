cf2dcb0efed394045d85fc6c059519fa6936f037
eptals1 <eptals2@gmail.com>
2025-08-13 10:19:51 +0800

    Update ResourceType.php

--- app/Enums/ResourceType.php
+++ app/Enums/ResourceType.php
@@ -2,16 +2,14 @@
 
 namespace App\Enums;
 
+use App\Enums\Traits\EnumHelper;
+
 enum ResourceType: string
 {
+    use EnumHelper;
     case MATERIALS = 'materials';
     case LABOR_EXPENSE = 'labor_expense';
     case EQUIPMENT_RENTAL = 'equipment_rental';
     case MISCELLANEOUS_COST = 'miscellaneous_cost';
     case OTHER_EXPENSES = 'other_expenses';
-
-    public static function values(): array
-    {
-        return array_column(self::cases(), 'value');
-    }
 }
