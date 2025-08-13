b1436a9e4ec20fdf93ed7841d5cc5d9938adf52b
eptals1 <eptals2@gmail.com>
2025-08-13 13:59:42 +0800

    Update ProjectAttachmentController.php

--- app/Http/Controllers/Api/V1/Project/ProjectAttachmentController.php
+++ app/Http/Controllers/Api/V1/Project/ProjectAttachmentController.php
@@ -34,22 +34,17 @@ class ProjectAttachmentController extends Controller
             }
             $storedFiles = [];
             foreach ($request->file('attachments') as $file) {
-                $filename = hash('sha256', $file->getClientOriginalName()) . '.' . $file->getClientOriginalExtension();
-                $path = "project/attachments/{$project->id}";
-                $fullPath = "{$path}/{$filename}";
-                Storage::disk('public')->put($fullPath, file_get_contents($file));
-                $project->attachments()->create([
+                $uniqueName = (string) Str::uuid(). '.' . $file->getClientOriginalExtension();
+                $originalName = $file->getClientOriginalName();
+                $path = "project/attachments/{$project->id}/{$uniqueName}";
+                Storage::disk('public')->put($path, file_get_contents($file));
+                $attachment = $project->attachments()->create([
                     'project_id' => $project->id,
-                    'name' => $filename,
-                    'path' => $fullPath,
+                    'name' => $originalName,
+                    'path' => $path,
                     'mime_type' => $file->getMimeType(),
                     ]);
-                $storedFiles[] = [
-                    'project_id' => $project->id,
-                    'name' => $filename,
-                    'path' => $fullPath,
-                    'mime_type' => $file->getMimeType(),
-                ];
+                $storedFiles[] = $attachment;
             }
             return response()->json([
                 'success' => true,
@@ -67,14 +62,6 @@ class ProjectAttachmentController extends Controller
 
     public function getDocumentViewerLink(Request $request, Project $project)
     {
-        $attachments = $project->attachments()->get();
-        if ($attachments->isEmpty()) {
-            return response()->json([
-                'success' => false,
-                'message' => 'No attachments found',
-                'data' => [],
-            ], 404);
-        }
         $uniqueKey = Str::random(15);
         Cache::put($uniqueKey, $project->id, now()->addMinutes(10));
         $webViewerUrl = route('web.document.viewer', ['cacheKey' => $uniqueKey]);
