582dfdde51ef6ad24b29e0bf3b7272471d2f6f3f
eptals1 <eptals2@gmail.com>
2025-08-20 10:30:38 +0800

    Create ChangeRequestStatus.php

---
+++ app/Enums/ChangeRequestStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use App\Enums\Traits\EnumHelper;
+
+enum ChangeRequestStatus: string
+{
+    use EnumHelper;
+    case PENDING = 'pending';
+    case APPROVED = 'approved';
+    case DECLINED = 'declined';
+}
+
+
