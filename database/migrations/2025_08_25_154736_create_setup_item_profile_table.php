19ac42a9c1d89007788ed1101dc15090c2ee3d25
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-08-26 10:59:54 +0800

    Update 2025_08_25_154736_create_setup_item_profile_table.php

--- database/migrations/2025_08_25_154736_create_setup_item_profile_table.php
+++ database/migrations/2025_08_25_154736_create_setup_item_profile_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +13,6 @@ return new class () extends Migration {
         Schema::dropIfExists('item_profiles');
         Schema::create('setup_item_profile', function (Blueprint $table) {
             $table->id();
-
             $table->string('item_code')->unique();
             $table->string('item_description')->nullable();
             $table->string('thickness')->nullable();
