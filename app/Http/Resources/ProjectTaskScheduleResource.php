49e0749839c5336afb6e376a8f7a4ba2ede5c3cd
eptals1 <eptals2@gmail.com>
2025-08-29 16:44:07 +0800

    Update ProjectTaskScheduleResource.php

--- app/Http/Resources/ProjectTaskScheduleResource.php
+++ app/Http/Resources/ProjectTaskScheduleResource.php
@@ -15,6 +15,7 @@ class ProjectTaskScheduleResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->name,
             'phases' => PhasesDetailedResource::collection($this->whenLoaded('phases')),
         ];
