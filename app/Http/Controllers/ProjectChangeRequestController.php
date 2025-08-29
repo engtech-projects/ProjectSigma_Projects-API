f67ea0edf1a28ca96f6c36ba1c3cbbf131f3e6e4
eptals1 <eptals2@gmail.com>
2025-08-29 16:37:54 +0800

    Update ProjectChangeRequestController.php

--- app/Http/Controllers/ProjectChangeRequestController.php
+++ app/Http/Controllers/ProjectChangeRequestController.php
@@ -7,56 +7,54 @@ use App\Http\Requests\UpdateProjectChangeRequest;
 use App\Http\Resources\ProjectChangeRequestResource;
 use App\Models\ProjectChangeRequest;
 
-
 class ProjectChangeRequestController extends Controller
 {
     public function index()
     {
-        $projectChangeRequest = ProjectChangeRequest::all();
+        $changeRequests = ProjectChangeRequest::all();
         return response()->json([
             'success' => true,
             'message' => 'Project change requests retrieved successfully',
-            'data' => ProjectChangeRequestResource::collection($projectChangeRequest),
+            'data' => ProjectChangeRequestResource::collection($changeRequests),
         ], 200);
     }
 
     public function store(StoreProjectChangeRequest $request)
     {
-        $projectChangeRequest = ProjectChangeRequest::create($request->validated());
+        $changeRequest = ProjectChangeRequest::create($request->validated());
         return response()->json([
             'success' => true,
             'message' => 'Project change request created successfully',
-            'data' => $projectChangeRequest,
+            'data' => new ProjectChangeRequestResource($changeRequest),
         ], 201);
     }
 
-    public function show($id)
+    public function show(ProjectChangeRequest $changeRequest)
     {
-        $projectChangeRequest = ProjectChangeRequest::findOrFail($id);
         return response()->json([
             'success' => true,
             'message' => 'Project change request retrieved successfully',
-            'data' => $projectChangeRequest,
+            'data' => new ProjectChangeRequestResource($changeRequest),
         ], 200);
     }
 
-    public function update(UpdateProjectChangeRequest $request, $id)
+    public function update(UpdateProjectChangeRequest $request, ProjectChangeRequest $changeRequest)
     {
-        $projectChangeRequest = ProjectChangeRequest::update($id, $request->validated());
+        $changeRequest->update($request->validated());
         return response()->json([
             'success' => true,
             'message' => 'Project change request updated successfully',
-            'data' => $projectChangeRequest,
+            'data' => new ProjectChangeRequestResource($changeRequest),
         ], 200);
     }
 
-    public function destroy($id)
+    public function destroy(ProjectChangeRequest $changeRequest)
     {
-        $projectChangeRequest = ProjectChangeRequest::destroy($id);
+        $changeRequest->delete();
         return response()->json([
             'success' => true,
             'message' => 'Project change request deleted successfully',
-            'data' => $projectChangeRequest,
+            'data' => new ProjectChangeRequestResource($changeRequest),
         ], 200);
     }
 
