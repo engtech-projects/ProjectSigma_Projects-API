e3e23b8c579a4de50075f8779f05888b644e0fbb
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-09-02 10:35:35 +0800

    [Update]

--- app/Models/ResourceItem.php
+++ app/Models/ResourceItem.php
@@ -1,21 +1,17 @@
 <?php
-
 namespace App\Models;
-
 use App\Enums\ResourceType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-
 class ResourceItem extends Model
 {
     use HasFactory;
     use SoftDeletes;
-
     protected $table = 'resources';
-
     protected $fillable = [
         'task_id',
         'resource_type',
@@ -32,39 +28,50 @@ class ResourceItem extends Model
         'work_time_category',
         'remarks',
     ];
-
     protected $casts = [
         'resource_type' => ResourceType::class,
     ];
-
     protected static function boot()
     {
         parent::boot();
-
         static::creating(function ($model) {
             if (empty($model->uuid)) {
                 $model->uuid = (string) Str::uuid();
             }
         });
     }
-
     public function task(): BelongsTo
     {
         return $this->belongsTo(BoqItem::class, 'task_id', 'id');
     }
-
     public function project(): BelongsTo
     {
         return $this->belongsTo(Project::class, 'project_id', 'id');
     }
-
     public function scopeFilterByTaskId($query, $taskId)
     {
         return $query->where('task_id', 'like', "%{$taskId}%");
     }
-
     public function scopeFilterByResourceType($query, $resourceType)
     {
         return $query->where('resource_type', 'like', "%{$resourceType}%");
+    }
+    public function cascadeUnitCostToOtherResourceItemsWithSameProjectAndUnit(): void
+    {
+        if ($this->resource_type !== ResourceType::MATERIALS) {
+            return;
+        }
+        $projectId = $this->task->part->project_id;
+        self::whereHas('task.part', function ($query) use ($projectId) {
+            $query->where('project_id', $projectId);
+        })
+            ->where('resource_type', $this->resource_type)
+            ->where('unit', $this->unit)
+            ->where('description', $this->description)
+            ->where('id', '!=', $this->id)
+            ->update([
+                'unit_cost'  => $this->unit_cost,
+                'total_cost' => DB::raw('quantity * ' . (float) $this->unit_cost),
+            ]);
     }
 }
--- app/Services/ResourceService.php
+++ app/Services/ResourceService.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace App\Services;
-
 use App\Models\Project;
 use App\Models\ResourceItem;
 use App\Models\BoqItem;
 use Illuminate\Support\Facades\DB;
-
 class ResourceService
 {
     public static function withPagination($request)
@@ -16,25 +13,20 @@ class ResourceService
             return $query->where('name', 'LIKE', "%{$request['key']}%");
         });
         $query->with('tasks');
-
         return $query->get();
     }
-
     public static function withProjects($request)
     {
         $query = ResourceItem::query();
         $query = $query->when(isset($request['key']), function ($query) use ($request) {
             return $query->where('name', 'LIKE', "%{$request['key']}%");
         });
-
         return $query->paginate(config('services.pagination.limit'));
     }
-
     public static function all()
     {
         return ResourceItem::all();
     }
-
     public static function create($request)
     {
         return DB::transaction(function () use ($request) {
@@ -44,6 +36,7 @@ class ResourceService
                 $request['total_cost'] = $request['quantity'] * $request['unit_cost'];
             }
             $data = ResourceItem::create($request);
+            $data->cascadeUnitCostToOtherResourceItemsWithSameProjectAndUnit();
             $task = BoqItem::findOrFail($request['task_id'])->load(['resources', 'phase']);
             if ($task->can_update_total_amount) {
                 $task->update([
@@ -54,7 +47,6 @@ class ResourceService
             return $data;
         });
     }
-
     public static function update($request, $id)
     {
         return DB::transaction(function () use ($request, $id) {
@@ -75,7 +67,6 @@ class ResourceService
             return $data;
         });
     }
-
     public static function delete($id)
     {
         return DB::transaction(function () use ($id) {
@@ -91,7 +82,6 @@ class ResourceService
             return $data;
         });
     }
-
     public static function show($id)
     {
         return ResourceItem::findOrFail($id);
