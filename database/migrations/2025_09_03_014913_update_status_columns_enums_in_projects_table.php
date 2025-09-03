e2a5574bc202c79a2fe1a532c65f1704b45cca62
eptals1 <eptals2@gmail.com>
2025-09-03 10:25:31 +0800

    Create 2025_09_03_014913_update_status_columns_enums_in_projects_table.php

---
+++ database/migrations/2025_09_03_014913_update_status_columns_enums_in_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Enums\ProjectStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->enum('status', ProjectStatus::toArray())
+                ->default('pending')
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->enum('status', ['pending','open','submitted','approved','ongoing','archived','on-hold','cancelled','void','deleted','draft','completed','myProjects'])
+                ->default('open')
+                ->change();
+        });
+    }
+};
