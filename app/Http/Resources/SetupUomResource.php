098c78b5b702101cc9834168e914fe390307dd20
Ephraim <eptals2@gmail.com>
2025-09-30 10:26:18 +0800

    Add symbol field to SetupUom model and related files

--- app/Http/Resources/SetupUomResource.php
+++ app/Http/Resources/SetupUomResource.php
@@ -17,6 +17,7 @@ class SetupUomResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'symbol' => $this->symbol
         ];
     }
 }
--- database/migrations/2025_09_29_032439_create_setup_uom_table.php
+++ database/migrations/2025_09_29_032439_create_setup_uom_table.php
@@ -13,6 +13,7 @@ return new class () extends Migration {
         Schema::create('setup_uoms', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('symbol');
             $table->timestamps();
             $table->softDeletes();
         });
