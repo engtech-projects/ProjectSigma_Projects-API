3dd05c6d215926dc151c1908efe5cb4192ba7c25
Ephraim <eptals2@gmail.com>
2025-10-06 08:55:04 +0800

    [UPDATE] Move symbol column to separate migration for setup_uoms

--- database/migrations/2025_10_06_004803_add_symbol_column_to_setup_uoms_table.php
+++ database/migrations/2025_10_06_004803_add_symbol_column_to_setup_uoms_table.php
@@ -4,17 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('setup_uoms', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
-            $table->softDeletes();
+        Schema::table('setup_uoms', function (Blueprint $table) {
+            $table->string('symbol', 10)->after('name');
         });
     }
 
@@ -23,6 +20,8 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('setup_uoms');
+        Schema::table('setup_uoms', function (Blueprint $table) {
+            $table->dropColumn('symbol');
+        });
     }
 };
