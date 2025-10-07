f1c4fb157d21f6c9936cf47f3b04cd097f24ff01
Ephraim <eptals2@gmail.com>
2025-10-07 14:46:31 +0800

    Update BoqItem.php

--- app/Models/BoqItem.php
+++ app/Models/BoqItem.php
@@ -67,7 +67,7 @@ class BoqItem extends Model
     }
     public function getFormattedQuantityAttribute()
     {
-        return number_format($this->quantity, 8);
+        return number_format($this->quantity, 2);
     }
     protected function getCanUpdateTotalAmountAttribute()
     {
