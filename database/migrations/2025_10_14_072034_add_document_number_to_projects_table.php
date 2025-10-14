b5eeed5ca2602bf348453f15e0e59f6688bb881f
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-10-14 17:42:16 +0800

    [FIX]

--- database/migrations/2025_10_14_072034_add_document_number_to_projects_table.php
+++ database/migrations/2025_10_14_072034_add_document_number_to_projects_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class () extends Migration {
     /**
      * Run the migrations.
@@ -11,7 +9,7 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->string('document_number')->after('cashflow')->nullable();
+            $table->string('document_number')->after('cash_flow')->nullable();
         });
     }
     public function down(): void
---
+++ database/migrations/2025_10_14_093338_update_acbtonumeric_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('projects')
+        ->whereNotNull('abc')
+        ->where('abc', '!=', '')
+        ->whereRaw("abc NOT REGEXP '^-?[0-9]+(\\.[0-9]+)?$'")
+        ->update(['abc' => '0']);
+        DB::statement("UPDATE projects SET abc = CAST(abc AS DECIMAL(20,8)) WHERE abc REGEXP '^-?[0-9]+(\\.[0-9]+)?$'");
+        Schema::table('projects', function (Blueprint $table) {
+            $table->decimal('abc', 20, 8)->nullable()->change();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement("
+            UPDATE projects
+            SET abc = CAST(abc AS CHAR)
+            WHERE abc IS NOT NULL
+        ");
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('abc')->nullable()->change();
+        });
+    }
+};
