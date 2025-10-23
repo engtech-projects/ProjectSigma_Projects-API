3a16f3d5f010a2b63a0705fa51112b14190a6d36
Ephraim <eptals2@gmail.com>
2025-10-23 10:04:25 +0800

    Update TaskSchedule.php

--- app/Models/TaskSchedule.php
+++ app/Models/TaskSchedule.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\TimelineClassification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -22,6 +23,7 @@ class TaskSchedule extends Model
     ];
 
     protected $casts = [
+        'timeline_classification' => TimelineClassification::class,
         'weight_percent' => 'decimal:2',
     ];
 
