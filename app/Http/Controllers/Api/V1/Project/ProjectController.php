b1490647c736423dfd12a80eb1aaf90da49a303e
Ephraim <eptals2@gmail.com>
2025-11-14 15:34:06 +0800

    Update ProjectController.php

--- app/Http/Controllers/Api/V1/Project/ProjectController.php
+++ app/Http/Controllers/Api/V1/Project/ProjectController.php
@@ -17,7 +17,6 @@ use App\Http\Resources\Project\ProjectDetailResource;
 use App\Http\Resources\Project\ProjectListingResource;
 use App\Http\Resources\Project\ProjectLiveDetailResource;
 use App\Http\Resources\Project\ProjectLiveListingResource;
-use App\Http\Resources\ProjectCompletionReportResource;
 use App\Http\Resources\ProjectDataSheetResource;
 use App\Http\Resources\SummaryOfDirectEstimateResource;
 use App\Models\Project;
@@ -233,14 +232,6 @@ class ProjectController extends Controller
             ->additional([
                 'success' => true,
                 'message' => 'Successfully fetched data sheet.',
-            ]);
-    }
-    public function getCompletionReport(Project $project)
-    {
-        return ProjectCompletionReportResource::make($project)
-            ->additional([
-                'success' => true,
-                'message' => 'Successfully fetched completion report.',
             ]);
     }
 }
