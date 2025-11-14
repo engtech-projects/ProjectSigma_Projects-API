9be725a4c72e4d8eec94bcd6d46993f79693c752
Ephraim <eptals2@gmail.com>
2025-11-14 14:37:43 +0800

    pint

--- app/Http/Controllers/Api/V1/Project/ProjectController.php
+++ app/Http/Controllers/Api/V1/Project/ProjectController.php
@@ -42,8 +42,8 @@ class ProjectController extends Controller
         $projectKey = $validated['project_key'] ?? null;
         $status = $validated['stage_status'] ?? null;
         $data = Project::with('revisions')
-            ->when($status, fn($query) => $query->filterByStage($status))
-            ->when($projectKey, fn($query) => $query->projectKey($projectKey))
+            ->when($status, fn ($query) => $query->filterByStage($status))
+            ->when($projectKey, fn ($query) => $query->projectKey($projectKey))
             ->latestFirst()
             ->paginate(config('services.pagination.limit'));
         return ProjectListingResource::collection($data)
@@ -58,8 +58,8 @@ class ProjectController extends Controller
         $projectKey = $validated['project_key'] ?? null;
         $status = $validated['stage_status'] ?? null;
         $data = Project::with('revisions')
-            ->when($status, fn($query) => $query->filterByStage($status))
-            ->when($projectKey, fn($query) => $query->projectKey($projectKey))
+            ->when($status, fn ($query) => $query->filterByStage($status))
+            ->when($projectKey, fn ($query) => $query->projectKey($projectKey))
             ->latestFirst()
             ->createdByAuth()
             ->paginate(config('services.pagination.limit'));
@@ -133,7 +133,7 @@ class ProjectController extends Controller
         $validated = $request->validated();
         $projectKey = $validated['project_key'] ?? null;
         $data = Project::ongoing()
-            ->when($projectKey, fn($query) => $query->projectKey($projectKey))
+            ->when($projectKey, fn ($query) => $query->projectKey($projectKey))
             ->latestFirst()
             ->paginate(config('services.pagination.limit'));
         return ProjectLiveListingResource::collection($data)
